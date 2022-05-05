ffba7f56139bbf36c7e036caa02b01fa9be301da
Kevin <1272006592@qq.com>
2022-05-05 13:33:09 +0800

    Updates

--- src/common/Api/api.js
+++
@@ -1,4 +0,0 @@
-const weatherApi =
-  "http://www.tianqiapi.com/api?version=v9&appid=23035354&appsecret=8YvlPNrz";
-
-export { weatherApi };
--- src/views/lifecycle/components/contextPage/createContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const ThemeContext = createContext("light");
-const SizeContext = createContext();
-
-export { ThemeContext, SizeContext };
